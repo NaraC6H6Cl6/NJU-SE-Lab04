de12cbd1a18cfa60db32bcdb4a97a9aeb2ef3e28
C6H6Cl6 <812972725@qq.com>
2022-11-17 00:51:32 +0800

    Test Test Test

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,35 +1,38 @@
 #include "Test.hpp"
 
+#include <iostream>
+
 
 /**
  * ! WARNING:
  * ! THE FOLLOWING PROCEDURES WILL BE CLEARED IN RELEASE VERSION! 
  */
 
+
 //  Reserved for Input Module
 void
 Sirius::TestProcedure1()
 {
-
+  std::cout << "Test Procedure 1 Completed" << std::endl;
 }
 
 //  Reserved for Equivalence Judger Module
 void
 Sirius::TestProcedure2()
 {
-
+  std::cout << "Test Procedure 2 Completed" << std::endl;
 }
 
 //  Reserved for Equivalence Confirmation Module
 void
 Sirius::TestProcedure3()
 {
-
+  std::cout << "Test Procedure 3 Completed" << std::endl;
 }
 
 // Reserved for Output Module
 void
 Sirius::TestProcedure4()
 {
-
+  std::cout << "Test Procedure 4 Completed" << std::endl;
 }
