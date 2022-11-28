4d3a746134971ba849451826aeab7985e3b7ae69
C6H6Cl6 <812972725@qq.com>
2022-11-28 15:43:29 +0800

    Commit

--- src/InputProcedures.cpp
+++ src/InputProcedures.cpp
@@ -9,95 +9,13 @@
  * 
  */
 
-#include "InputModule.hpp"
+#include "InputProcedures.hpp"
+#include "InputGroups.hpp"
 #include "InputType.hpp"
 #include <filesystem>
-#include <fstream>
 #include <regex>
 #include <iostream>
 
-
-
-std::shared_ptr<Phoenix::InputGroup>
-Phoenix::InputGroup::MakeInputGroup(const std::filesystem::path& DirectoryPath)
-{
-  std::error_code ErrorCode;
-
-  bool bIsDirectory = std::filesystem::is_directory(DirectoryPath, ErrorCode);
-  if (!bIsDirectory) {
-    return nullptr;
-  }
-
-  std::filesystem::path TempStdinFormatFile;
-  std::vector<std::filesystem::path> TempSourceProgramFiles;
-  for (auto& ChildDirectory : std::filesystem::directory_iterator(DirectoryPath)) {
-    auto& ChildPath = ChildDirectory.path();
-    if (ChildPath.filename() == "stdin_format.txt") {
-
-      TempStdinFormatFile = ChildPath;
-
-    } else if (CompilableFileExtensions.find(std::string(ChildPath.extension()))
-        != CompilableFileExtensions.end()
-      ) {
-
-      TempSourceProgramFiles.push_back(ChildPath);
-    }
-
-  }
-
-  if (TempStdinFormatFile != "" && TempSourceProgramFiles.size() > 0) {
-    Phoenix::InputGroup o;
-    o.StdinFormatFile = std::move(TempStdinFormatFile);
-    o.SourceProgramFiles = std::move(TempSourceProgramFiles);
-    return std::make_shared<Phoenix::InputGroup>(std::move(o));
-  } else {
-    return nullptr;
-  }
-
-}
-
-
-const std::filesystem::path&
-Phoenix::InputGroup::GetStdinFormatFile() const
-{
-  return this->StdinFormatFile;
-}
-
-
-const std::vector<std::filesystem::path>&
-Phoenix::InputGroup::GetSourceProgramFiles() const
-{
-  return this->SourceProgramFiles;
-}
-
-
-const std::unordered_set<std::string>
-  Phoenix::InputGroup::CompilableFileExtensions = {
-    ".c", ".cc", ".cpp"
-  };
-
-
-Phoenix::InputGroups
-Phoenix::GetInputGroups()
-{
-  Phoenix::InputGroups Groups;
-
-  std::filesystem::path CurrentPath = std::filesystem::current_path();
-
-  for (auto& ChildDirectory : std::filesystem::directory_iterator(CurrentPath)) {
-    auto& ChildPath = ChildDirectory.path();
-    if (std::filesystem::is_directory(ChildPath)) {
-
-      auto p = Phoenix::InputGroup::MakeInputGroup(ChildPath);
-      if (p != nullptr) {
-        Groups.emplace_back(p);
-      }
-
-    }
-  }
-
-  return Groups;
-}
 
 
 std::shared_ptr<Mercury::InputType>
@@ -138,57 +56,30 @@ Phoenix::MakeInputByToken(std::string Token)
 
   } else {
     return nullptr;
-
   }
 
 }
 
 
-Phoenix::InputContents
-Phoenix::MakeInputs(Phoenix::InputGroups& Groups)
+
+std::vector<std::shared_ptr<Plum::InputGroup>>
+Phoenix::GetInputGroups()
 {
-  Phoenix::InputContents Contents;
-
-  for (auto Group : Groups) {
-    std::shared_ptr<Mercury::InputType> TempInputType;
-    std::vector<std::shared_ptr<Mercury::InputType>> InputTypes;
-
-    const auto& StdinFormatFilePath = Group->GetStdinFormatFile();
-    std::ifstream StdinFormatFileStream(StdinFormatFilePath);
-
-    if (StdinFormatFileStream.is_open()) {
-      // Successfully opened the file
-      std::string Token;
-      while (StdinFormatFileStream >> Token) {
-        TempInputType = Phoenix::MakeInputByToken(Token);
-        if (TempInputType == nullptr) {
-          // Failed to analyze token
-          StdinFormatFileStream.close();
-          goto AnalyzationFailed;
-        }
+  std::vector<std::shared_ptr<Plum::InputGroup>> Groups;
+  // Get the path of the application.
+  std::filesystem::path CurrentPath = std::filesystem::current_path();
+  // Every child folder may be an input group.
+  for (auto& ChildDirectory : std::filesystem::directory_iterator(CurrentPath)) {
+    auto& ChildPath = ChildDirectory.path();
+    if (std::filesystem::is_directory(ChildPath)) {
+      auto p = Plum::InputGroup::MakeInputGroup(ChildPath);
+      // If the folder is a valid input group, the function will not return nullptr
+      if (p != nullptr) {
+        Groups.emplace_back(p);
       }
-    } else {
-      // Failed to open the file, so go to the next folder
-      continue;
     }
-    // Successfully analyzed one format
-    StdinFormatFileStream.close();
-    Contents.emplace_back(Group, std::move(InputTypes));
-    continue;
-AnalyzationFailed:
-    StdinFormatFileStream.close();
-    continue;
   }
-
-  return Contents;
+  return Groups;
 }
 
-
-Phoenix::InputContents
-Phoenix::InputProcedure()
-{
-  auto Groups = GetInputGroups();
-  auto Contents = MakeInputs(Groups);
-  return Contents;
-}
 
