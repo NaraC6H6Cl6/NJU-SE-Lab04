27a71f9b1a7ed88c2666b8c9acee34d4479f86ed
C6H6Cl6 <812972725@qq.com>
2022-11-29 23:31:27 +0800

    Release

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,16 @@
-#include "Test.hpp"
+#include "InputGroup.hpp"
+#include "InputProcedure.hpp"
+#include "EquivalenceJudgment.hpp"
+#include "OutputProcedure.hpp"
 #include <iostream>
 
 int
 main()
 {
-  Sirius::TestProcedure1();
-  Sirius::TestProcedure2();
-  Sirius::TestProcedure3();
-  Sirius::TestProcedure4();
+  const auto& Groups = Phoenix::GetInputGroups();
+  for (auto& Group : Groups) {
+    const auto& Set = Hikari::EquivalenceJudgement(*Group);
+    Violet::OutputResult(*Group, Set);
+  }
   return 0;
 }
